4e93b6330e8fbafb9210ceb80c2ee241093a9e6c
schemabuoi <43758232+schemabuoi@users.noreply.github.com>
2019-02-01 16:37:42 +0100

    Update LoginViewController.java
    Implementing clickForgotPassword method

--- AttendanceAutomation/src/attendanceautomation/gui/controller/LoginViewController.java
+++ AttendanceAutomation/src/attendanceautomation/gui/controller/LoginViewController.java
@@ -5,19 +5,29 @@
  */
 package attendanceautomation.gui.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import java.net.URL;
+import java.util.EventObject;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -29,10 +39,10 @@ public class LoginViewController implements Initializable {
     @FXML
     private TextField txtUsername;
     @FXML
-    private JFXTextField txtPassword;
-    private Button btnLogin;
+    private JFXPasswordField txtPassword;
     @FXML
-    private AnchorPane paneLogin;
+    private JFXButton btnLogin;
+    
 
     /**
      * Initializes the controller class.
@@ -74,13 +84,12 @@ public class LoginViewController implements Initializable {
     }
 
     @FXML
-    private void clickLogin(ActionEvent event) 
+    private void clickForgotPassword(ActionEvent event) throws IOException 
     {
-        
-    }
-
-    @FXML
-    private void clickForgotPassword(ActionEvent event) {
+        Parent root = FXMLLoader.load(getClass().getResource("/attendanceautomation/gui/view/ForgotPasswordView.fxml"));
+        Scene currentScene = ((Node)((EventObject) event).getSource()).getScene();
+        Stage currentStage = (Stage)((Node)((EventObject) event).getSource()).getScene().getWindow();
+        currentStage.setScene(new Scene(root, currentScene.getWidth(), currentScene.getHeight()));
     }
     
 }
