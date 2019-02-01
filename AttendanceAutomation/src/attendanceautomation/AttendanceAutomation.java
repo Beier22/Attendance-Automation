5bb53261456789df5a4115d1a24b1c1ea0c5e737
schemabuoi <43758232+schemabuoi@users.noreply.github.com>
2019-02-01 16:38:07 +0100

    Update AttendanceAutomation.java
    Implementing window size restrictions + adding title and app image

--- AttendanceAutomation/src/attendanceautomation/AttendanceAutomation.java
+++ AttendanceAutomation/src/attendanceautomation/AttendanceAutomation.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -24,6 +25,11 @@ public class AttendanceAutomation extends Application {
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.setMinWidth(650);
+        stage.setMinHeight(530);
+        stage.setTitle("EASV Attendance System");
+        Image icon = new Image(getClass().getResourceAsStream("/attendanceautomation/gui/images/EASYDVEST.png"));
+        stage.getIcons().add(icon);
         stage.show();
     }
 
