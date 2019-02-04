d5ca1f1c84fd70750152e386d06a38fba57a62f9
schemabuoi <43758232+schemabuoi@users.noreply.github.com>
2019-02-04 17:55:48 +0100

    Update ForgotPasswordViewController.java
    Updating initialize method + implementing clickLogin method

--- AttendanceAutomation/src/attendanceautomation/gui/controller/ForgotPasswordViewController.java
+++ AttendanceAutomation/src/attendanceautomation/gui/controller/ForgotPasswordViewController.java
@@ -5,21 +5,35 @@
  */
 package attendanceautomation.gui.controller;
 
+import attendanceautomation.gui.util.AnimationUtil;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.EventObject;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.ParallelTransition;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -29,27 +43,94 @@ import javafx.stage.Stage;
 public class ForgotPasswordViewController implements Initializable {
 
     @FXML
-    private JFXButton btnLogin;
-    @FXML
     private AnchorPane ancMain;
     @FXML
     private JFXTextField txtUsername;
+    @FXML
+    private Label lblTitle;
+    @FXML
+    private Label lblFirstLine;
+    @FXML
+    private Label lblSecondLine;
+    @FXML
+    private JFXButton btnSend;
+    @FXML
+    private JFXButton btnLogin;
 
     /**
      * Initializes the controller class.
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        
-    }    
+    public void initialize(URL url, ResourceBundle rb) 
+    {
+        showElements();      
+        createTextFieldsListener();
+    }   
+    
+    private void createTextFieldsListener()
+    {
+        txtUsername.setOnKeyPressed(new EventHandler<KeyEvent>()
+                {
+                    @Override
+                    public void handle(KeyEvent key) 
+                    {
+                        if(key.getCode() == KeyCode.ENTER)
+                        {
+                            btnSend.fire();
+                        }
+                    }
+                    
+                }
+            );
+    }
+
+    
+    private void showElements()
+    {
+        List<Node> elements = new ArrayList();
+        elements.add(lblTitle);
+        elements.add(lblFirstLine);
+        elements.add(lblSecondLine);
+        elements.add(txtUsername);
+        ParallelTransition transition = AnimationUtil.createVerticalSlideWithFadingIn(-300, 0, elements);
+        transition.play();
+    }
 
     @FXML
     private void clickLogin(ActionEvent event) throws IOException 
     {
-       Parent root = FXMLLoader.load(getClass().getResource("/attendanceautomation/gui/view/LoginView.fxml"));
-       Scene currentScene = ((Node)((EventObject) event).getSource()).getScene();
-       Stage currentStage = (Stage)((Node)((EventObject) event).getSource()).getScene().getWindow();
-       currentStage.setScene(new Scene(root, currentScene.getWidth(), currentScene.getHeight()));
+        List<Node> elements = new ArrayList();
+        elements.add(lblTitle);
+        elements.add(lblFirstLine);
+        elements.add(lblSecondLine);
+        elements.add(txtUsername);
+        ParallelTransition transition = AnimationUtil.createVerticalSlideWithFadingOut(0, -300, elements);
+        
+        transition.setOnFinished(new EventHandler(){
+            @Override
+            public void handle(Event event) {
+                showLoginView();
+            }
+        });
+        
+        transition.play();
+        
+
+    }
+    
+    private void showLoginView()
+    {
+        try
+        {
+           Parent root = FXMLLoader.load(getClass().getResource("/attendanceautomation/gui/view/LoginView.fxml"));
+           Scene currentScene = btnSend.getScene();
+           Stage currentStage = (Stage)btnSend.getScene().getWindow();
+           currentStage.setScene(new Scene(root, currentScene.getWidth(), currentScene.getHeight()));
+        }
+        catch(IOException e)
+        {
+            
+        }
     }
     
 }
